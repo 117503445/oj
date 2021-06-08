761d43ce80d383d4775bbc1108825bfd98e97f9b
117503445 <t117503445@gmail.com>
2021-06-08 19:46:25 +0800

    feat: functional verification

--- main.go
+++ main.go
@@ -1,14 +1,36 @@
 package main
 
 import (
-	"flag"
+	"context"
 	"fmt"
+	"os/exec"
+	"strings"
+	"time"
 )
 
 func main() {
-	fmt.Println("1")
-	s := flag.String("s", "", "source code path")
-	fmt.Println(*s)
-	fmt.Println("1")
+	// Create a new context and add a timeout to it
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel() // The cancel should be deferred so resources are cleaned up
 
+	// Create the command with our context
+	cmd := exec.CommandContext(ctx, "./assets/success/build/main.exe")
+	cmd.Stdin = strings.NewReader("1 2")
+	// This time we can simply use Output() to get the result.
+	out, err := cmd.Output()
+
+	// We want to check the context error to see if the timeout was executed.
+	// The error returned by cmd.Output() will be OS specific based on what
+	// happens when a process is killed.
+	if ctx.Err() == context.DeadlineExceeded {
+		fmt.Println("Output:", string(out))
+		fmt.Println("Command timed out")
+		return
+	}
+
+	// If there's no context error, we know the command completed (or errored).
+	fmt.Println("Output:", string(out))
+	if err != nil {
+		fmt.Println("Non-zero exit code:", err)
+	}
 }
